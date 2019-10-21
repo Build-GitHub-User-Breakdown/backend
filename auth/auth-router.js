86015f84de4ec4a9985dd7e73c83c233a2215338
jtmccutcheon <jt.mccutcheon@live.com>
2019-10-21 16:06:05 -0400

    first commit, login, register endpoints work, working on github api

---
+++ auth/auth-router.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const Users = require('./auth-model')
+const secret = require('../config/secrets')
+
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved, 'successfully added user');
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "error adding user" });
+        });
+});
+
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = generateToken(user);
+                res.status(200).json({
+                    message: `Hi, ${user.username}!`,
+                    token
+                });
+            } else {
+                res.status(401).json({ message: "error logging in" });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "error logging in" });
+        });
+});
+
+function generateToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username
+    };
+    const options = {
+        expiresIn: '1d'
+    };
+    return jwt.sign(payload, secret.jwtSecret, options)
+}
+
+
+
+module.exports = router;
---
+++ github-users/github-users-router.js
@@ -0,0 +1,19 @@
+const axios = require('axios')
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    const requestOptions = {
+        headers: { accept: 'application/json' },
+    }
+
+    axios
+        .get('https://api.github.com', requestOptions)
+        .then(res => {
+            res.status(200).json(res)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'error on github api' });
+        });
+})
+
+module.exports = router;
