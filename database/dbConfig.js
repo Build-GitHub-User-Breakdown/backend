b9eeedbc963cfc1524dbf5c685a73170abf1453d
jtmccutcheon <jt.mccutcheon@live.com>
2019-10-24 15:43:20 -0400

    fixed delete endpoint

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -5,14 +5,3 @@ const knexConfig = require('../knexfile.js');
 
 module.exports = knex(knexConfig.development);
 
-router.delete('/:id', (req, res) => {
-    Favorites
-        .deleteFavorites(req.params.id)
-        .then(deleted => {
-            res.status(200).json(deleted)
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err);
-        })
-})
