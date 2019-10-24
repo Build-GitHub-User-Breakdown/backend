12b274ef53dd168b307afab0ca5ff46a460463a0
jtmccutcheon <jt.mccutcheon@live.com>
2019-10-24 15:41:29 -0400

    changing return on post favorite

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,5 +3,16 @@ const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
 
-module.exports = knex(knexConfig.production);
+module.exports = knex(knexConfig.development);
 
+router.delete('/:id', (req, res) => {
+    Favorites
+        .deleteFavorites(req.params.id)
+        .then(deleted => {
+            res.status(200).json(deleted)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err);
+        })
+})
